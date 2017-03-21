b76d82f4579bdf60570b2f32c8831c3c4cf7b80b
c-bata <contact@c-bata.link>
2017-03-21 13:37:15 +0900

    Update server

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
 import time
-from wsgiref.util import setup_testing_defaults
 from wsgiref.simple_server import make_server
 
 
-def slow_app(environ, start_response):
-    time.sleep(3)
+def app(environ, start_response):
+    time.sleep(1)
     start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'This is a very slow Web API']
+    return [b'This is a slow web api']
 
 
 if __name__ == '__main__':
     print("Serving on port 8000...")
-    with make_server('', 8000, slow_app) as httpd:
+    with make_server('', 8000, app) as httpd:
         httpd.serve_forever()
 
